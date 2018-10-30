d0feac1306068e18f41bb69772ec9b90e16ad475
Chukwu Gideon <chuksgid@gmail.com>
2018-10-30 02:24:02 +0100

    This is my first commit on this project

---
+++ app/src/main/java/com/example/chuks/calcc/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.chuks.calcc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
